458696291468331937cb4d2ae0023e5f0c79047e
EnigmaK9 <enigmak9@protonmail.com>
2024-05-13 00:02:47 -0600

    sql should work

--- 03-bases-de-datos/homework/04-data-warehouse/snowflake.sql
+++ 03-bases-de-datos/homework/04-data-warehouse/snowflake.sql
@@ -1,5 +1,5 @@
 -- creación de la dimensión almacén
-create table dimension_almacén (
+create table dimension_almacen (
     almacen_id integer primary key,
     nombre varchar(100),
     direccion varchar(100),
@@ -15,8 +15,10 @@ create table dimension_producto (
     categoria varchar(50),
     subcategoria varchar(50),
     marca varchar(50),
-    precio double,
-    altura double
+    precio double precision,
+    altura double precision,
+    marca_id integer,
+    foreign key (marca_id) references dimension_marca(marca_id)
 );
 
 -- creación de la dimensión marca
@@ -29,7 +31,7 @@ create table dimension_marca (
 create table dimension_tiempo (
     tiempo_id integer primary key,
     fecha date,
-    year integer,
+    anio integer,
     trimestre varchar(10),
     mes varchar(20),
     semana varchar(20),
@@ -45,33 +47,37 @@ create table dimension_cliente (
     fecha_registro date
 );
 
+-- creación de la tabla de dimensión tipo
+create table dimension_tipo (
+    tipo_id integer primary key,
+    descripcion varchar(100)
+);
+
 -- creación de la tabla dimensión promoción
 create table dimension_promocion (
     promocion_id integer primary key,
     nombre varchar(100),
-    tipo,
-    costo,
-    inicio,
-    fin,
-    tipo_id,
+    tipo varchar(50),
+    costo double precision,
+    inicio date,
+    fin date,
+    tipo_id integer,
     foreign key (tipo_id) references dimension_tipo(tipo_id)
-)
--- creación de la tabla de dimensión tipo
-create table dimension_tipo (
-    tipo_id integer primary key,
-    fecha date,
-    condiciones varchar(100)
-)
+);
 
 -- creación de la tabla de hechos ventas
 create table hechos_ventas (
     ventas_id integer primary key,
     producto_id integer,
+    almacen_id integer,
     tiempo_id integer,
     cliente_id integer,
+    promocion_id integer,
     unidades integer,
-    precio double,
+    precio double precision,
     foreign key (producto_id) references dimension_producto(producto_id),
+    foreign key (almacen_id) references dimension_almacen(almacen_id),
     foreign key (tiempo_id) references dimension_tiempo(tiempo_id),
-    foreign key (cliente_id) references dimension_cliente(cliente_id)
+    foreign key (cliente_id) references dimension_cliente(cliente_id),
+    foreign key (promocion_id) references dimension_promocion(promocion_id)
 );

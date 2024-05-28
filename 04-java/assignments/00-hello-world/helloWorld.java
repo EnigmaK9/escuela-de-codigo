1c6cb0884ed5583481d5874b99775ed6168c7faa
Ignacio Padilla <enigmak9@protonmail.com>
2024-05-28 14:40:31 -0600

    hello world program added

---
+++ 04-java/assignments/00-hello-world/helloWorld.java
@@ -0,0 +1,13 @@
+// Definimos una clase llamada HelloWorld
+public class helloWorld {
+
+    // Este es el método principal, el punto de entrada de cualquier aplicación Java.
+    // Es el método que se ejecuta al iniciar el programa.
+    public static void main(String[] args) {
+
+        // Utilizamos System.out.println para imprimir texto en la consola.
+        // En este caso, imprimimos "Hello, World!".
+        System.out.println("Hello, World!");
+    }
+}
+

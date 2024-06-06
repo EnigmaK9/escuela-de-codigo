90b86b9fff526f411b0595f2840e188143d890f3
EnigmaK9 <enigmak9@protonmail.com>
2024-06-05 23:58:09 -0600

    added arrow code

---
+++ 05-javascript/arrow.js
@@ -0,0 +1,36 @@
+
+
+### Lenguajes Populares en el Mundo del Desarrollo Web
+
+#### JavaScript:
+- **Diseñado para brindar dinamismo a las páginas web**
+- **Validar formularios y muchas cosas más para mejorar la experiencia del usuario**
+- **Manipular el Document Object Model (DOM)**
+- **Crear aspectos visuales dinámicos**
+- **Desarrollo de juegos 2D y 3D**
+- **Desarrollo de extensiones de navegador**
+- **Web apps y aplicaciones móviles**
+- **Desarrollo de servidores**
+
+### Características Nuevas y Mejoras en JavaScript:
+- **Nuevas características**
+- **Mejor rendimiento**
+
+### Metalenguajes:
+- **HTML**
+- **CSS**
+
+### Lenguajes de Programación:
+- **C**
+- **C++**
+- **VBA**
+- **Y otros**
+
+### Lenguajes de Propósito Específico:
+- **Orientados a una tarea concreta y específica**
+- **Facilitan operaciones en páginas web**
+// 
+### Intervalos de Desarrollo:
+1. **Intervalo de diseño de páginas web dinámicas**
+2. **Intervalo de mejora de la experiencia del usuario**
+3. **Intervalo de desarrollo de juegos y aplicaciones**

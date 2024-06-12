7105c778c78c573d13c220e5b384946562d88a31
enigmak9 <enigmak9@protonmail.com>
2024-06-12 11:21:41 -0600

    external js implemented

--- 05-javascript/functions/main.js
+++ 05-javascript/functions/main.js
@@ -1,24 +1,24 @@
-var inputUser = prompt("Ingresa un valor para sumar:");
-var a = parseInt(inputUser);
+        var inputUser = prompt("Type in the value of a: ");
+        var inputUser2 = prompt("Type in the value of b: ");
+        var a = parseInt(inputUser);
+        var b = parseInt(inputUser2);
+        let add_five_to_a = addFive(b);
+        let a_plus_seven = sum(a, 7);
+        let sum_of_a_plus_b = sum(a,b);
+        
+        
+        alertBox(`The sum of ${a} and 7 is: ${a_plus_seven}`);
+        alertBox(`The sum of ${b} + 5 is: ${add_five_to_a}`);
+        alertBox(`The sum of ${a} + ${b} is: ${sum_of_a_plus_b}`);
+        
+        function sum(x, y) {
+            return x + y;
+        }
 
-let result = sum(a, 7);
-alertBox("Este es un ejemplo");
-alertBox(${result});
+        function alertBox(text) {
+            window.alert(text);
+        }
 
-var inputUser2 = prompt("Ingresa tu valor x: ");
-var b = parseInt(inputUser2);
-let c = addFive(b);
-alertBox(El resultado de ${b} + 5 = ${c});
-
-function sum(x, y) {
-    return x + y;
-}
-
-function alertBox(text) {
-    window.alert(text);
-}
-
-function addFive(x) {
-    let y = x + 5;
-    return y;
-}
+        function addFive(x) {
+            let y = x + 5;
+            return y;
